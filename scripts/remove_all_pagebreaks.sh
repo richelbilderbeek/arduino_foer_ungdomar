5d87db2d6d1a15d5a55b86c237219b2f6a6d3159
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-01-09 05:27:23 +0100

    Script gives more debug info

--- scripts/remove_all_pagebreaks.sh
+++ scripts/remove_all_pagebreaks.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-find . -name "*.md" -type f -print0 | xargs sed -i -e '/\pagebreak/d'
+#
+# Remove all '\pagebreak',
+# so that they do not show up on the MkDocs website
+#
+# Usage:
+#
+#   ./scripts/remove_all_pagebreaks.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/remove_all_pagebreaks.sh"
+    echo " "
+    exit 42
+fi
+
+
+# find . -name "*.md" -type f -print0 | xargs sed -i -e '/\pagebreak/d'
+
+for filename in $(find . -name "*.md" -type f -print0)
+do
+  echo "Modifying file: ${filename}"
+  sed -i '/\pagebreak/d' "${filename}"
+done
+
+git status
+
